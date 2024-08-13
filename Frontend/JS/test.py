a1f9328cac24e5f6be2355fa6f58265bc1ae5b60
Jogang523 <gone523@naver.com>
2024-08-13 17:15:06 +0900

    Update test.py

--- Frontend/JS/test.py
+++ Frontend/JS/test.py
@@ -1,15 +1,3 @@
-numRows = input('번호')
-out=[]
-lis=[]
-for i in range(1, int(numRows)+1):
-    lis.append(1)
-    for j in range(0,i):
-        out.append(lis)
-print(out)
-        
+p='PPAAALLALLLLP'
 
-    #      1
-    #     1 1
-    #    1 3 1
-    #   1 4 4 1
-    #  1 5 8 5 1
+print('LLL' in p)
