7e157742d2b4164f4375a2bf812424b436a2d471
Jogang523 <gone523@naver.com>
2024-08-13 11:03:40 +0900

    Update test.py

--- Frontend/JS/test.py
+++ Frontend/JS/test.py
@@ -2,6 +2,14 @@ numRows = input('번호')
 out=[]
 lis=[]
 for i in range(1, int(numRows)+1):
-    lis.append(i)
-    out.append(lis[0:2])
+    lis.append(1)
+    for j in range(0,i):
+        out.append(lis)
 print(out)
+        
+
+    #      1
+    #     1 1
+    #    1 3 1
+    #   1 4 4 1
+    #  1 5 8 5 1
